3f3e28556f942e01958ea5823a495c3d2a1d8abd
mayong <mayong@beescm.cn>
2018-02-01 14:55:31 +0800

    最基础的视频播放  MediaPlayer+SurfaceView

--- app/src/main/java/com/example/mayong/androidvideodemo/MainActivity.java
+++ app/src/main/java/com/example/mayong/androidvideodemo/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.mayong.androidvideodemo;
 
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String path = "http://112.253.22.157/17/z/z/y/u/zzyuasjwufnqerzvyxgkuigrkcatxr/hc.yinyuetai.com/D046015255134077DDB3ACA0D7E68D45.flv";
+    private MediaPlayer mediaPlayer;
+    private SurfaceView surfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        try {
+            init();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initView() {
+        surfaceView = findViewById(R.id.surface);
+    }
+
+    private void init() throws IOException {
+        mediaPlayer = new MediaPlayer();
+
+        mediaPlayer.setDataSource(this, Uri.parse(path));
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                mediaPlayer.setDisplay(holder);
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
+        mediaPlayer.prepare();
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mediaPlayer.start();
+                mediaPlayer.setLooping(true);
+            }
+        });
     }
 }

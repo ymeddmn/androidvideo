0e680842abbc5c85700b3570989b9bcc36fe67c7
mayong <mayong@beescm.cn>
2018-02-05 13:04:43 +0800

    最基础的视频播放  MediaPlayer+SurfaceView

--- app/src/main/java/com/example/mayong/androidvideodemo/MainActivity.java
+++ app/src/main/java/com/example/mayong/androidvideodemo/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.mayong.androidvideodemo;
 
-import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
+import android.widget.VideoView;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
